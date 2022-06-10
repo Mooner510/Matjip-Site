b858945b26e324d6f3bb6017069e1fb989deff51
wjknnn <wjknn3123@gmail.com>
2022-06-10 17:25:56 +0900

    keyframes fix

--- moonsunghwa/src/App.js
+++ moonsunghwa/src/App.js
@@ -1,29 +1,85 @@
-import React, { useState } from 'react';
-import Users from './Users';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Users from './Users.js'
 import './App.css';
 
 function App() {
-  const [state, setState] = useState('');
+  const [state, setState] = useState("");
+  const [lastState, setLastState] = useState("");
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [headerAni, setHeader] = useState({
+    backgroundImage: "url(/images/backgroundimg.jpg)"
+  });
+  const [matjip, setMatjip] = useState({});
 
-  const handleChange = (e) => {
-      setState(e.target.value);
+  const reloads = (e) => {
+    setState("/api/restaurant/search?matjip=" + e.target.value);
+    setHeader({
+      backgroundImage: "url(/images/backgroundimg.jpg)",
+      animation: 'visi 0.5s forwards'
+    });
+    setMatjip({
+      animation: 'opa 0.5s forwards'
+    });
+  }
+
+  const fetchUsers = async () => {
+      try {
+          setError(null);
+          setLoading(true);
+          if(state != null && state.length > 0) {
+            if(lastState !== state) {
+              setLastState(state);
+              const response = await axios.get(state, );
+              setUsers(response.data);
+            }
+          } else {
+            setUsers(null);
+          }
+      } catch (e) {
+          setError(e);
+          console.log(e);
+      }
+      setLoading(false);
   };
+
+  useEffect(() => {
+    fetchUsers();
+  });
+
+  let query;
+  if(loading) query = "로딩중...";
+  else if(error) query = "에러가 발생했습니다";
+  else if(!users) query = null;
+  else query = users.searchResult.map(user => (
+    <Users
+      title={user.title}
+      category={user.category}
+      address={user.address}
+      roadAddress={user.roadAddress}
+      homePageLink={user.homePageLink}
+      imageLink={user.imageLink}
+    />
+  ));
+
   return (
-    <>
-      <header style={{backgroundImage: "url(/images/backgroundimg.jpg)"}}>
+    <body>
+      <header style={headerAni}>
           <div className="topbar">
               <a href="#top"><button className="menubutton"><img src={'/images/list.png'} alt="" style={{height: 30}}/></button></a>
-              <input type="text" onChange={handleChange}/>
-              <a href="#top"><button className="loginbutton">LOGIN</button></a>
+              <input type="text" placeholder='음식을 입력하세요' onKeyPress={reloads}/>
+              <a href="#top" onClick={reloads}><button className="loginbutton">검색</button></a>  
           </div>
-          <div className='Matjip'>
+          <div className='Matjip' style={matjip}>
               <h1 className='txt'>맛집</h1>
               <h2 className='txt'>MATJIP</h2>
           </div>
       </header>
-      <Users matjip={state.text}/>
-    </>
+      {query}
+    </body>
   );
-}
+ }
 
 export default App;
--- moonsunghwa/src/Users.js
+++ moonsunghwa/src/Users.js
@@ -1,45 +1,61 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
+import './Users.css'
 
 function Users(props) {
-    const URL = `/api/restaurant/search?matjip=${props.matjip}`;
+    return(
+        <div className='matjip'>
+            <div className='info'>
+                <div className='imgdiv'>
+                    <img src={props.imageLink} alt='' style={{width: '100%'}}/>
+                </div>
+                <div>
+                    <h2>{props.title}</h2>
+                    <h4>{props.category}</h4>
+                    <h4>{props.address}</h4>
+                    <h4>{props.roadAddress}</h4>
+                </div>
+            </div>
+            <button className='mjbutton'>맛집추가</button>
+        </div>
+    )
+    // const URL = `/api/restaurant/search?matjip=${props.matjip}`;
 
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    // const [users, setUsers] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    // const [error, setError] = useState(null);
 
-    const fetchUsers = async () => {
-        try {
-            setError(null);
-            setUsers(null);
-            setLoading(true);
-            const response = await axios.get(URL, );
-            setUsers(response.data);
-        } catch (e) {
-            setError(e);
-            console.log(e);
-        }
-        setLoading(false);
-    };
+    // const fetchUsers = async () => {
+    //     try {
+    //         setError(null);
+    //         setUsers(null);
+    //         setLoading(true);
+    //         const response = await axios.get(URL, );
+    //         setUsers(response.data);
+    //     } catch (e) {
+    //         setError(e);
+    //         console.log(e);
+    //     }
+    //     setLoading(false);
+    // };
 
-    useEffect(() => {
-        fetchUsers();
-    }, []);
+    // useEffect(() => {
+    //     fetchUsers();
+    // }, []);
 
-    if(loading) return <div>로딩중..</div>;
-    if(error) return <div>에러가 발생했습니다</div>;
-    if(!users) return null;
-    return (
-        <ul>
-            {users.searchResult.map(user => (
-                <>
-                    <li>가게이름 : {user.title}</li>
-                    <li>카테고리 : {user.category}</li>
-                    <li>주소 : {user.address}</li>
-                </>
-            ))}
-        </ul>
-    );
+    // if(loading) return <div>로딩중..</div>;
+    // if(error) return <div>에러가 발생했습니다</div>;
+    // if(!users) return null;
+    // return (
+    //     <ul>
+    //         {users.searchResult.map(user => (
+    //             <>
+    //                 <li>가게이름 : {user.title}</li>
+    //                 <li>카테고리 : {user.category}</li>
+    //                 <li>주소 : {user.address}</li>
+    //             </>
+    //         ))}
+    //     </ul>
+    // );
 }
 
 export default Users;
